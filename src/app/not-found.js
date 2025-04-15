8642e2c82ed91c9ebd610b05d3ab0f50ca807cd4
Henry <tongsik98@gmail.com>
2025-04-15 00:46:50 -0700

    add import Link to 404

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 export default function NotFound({ message }) {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-5xl font-bold mb-4">404 - Page Not Found</h1>
-      <p className="text-lg text-gray-600 mb-8">{message ? message : "sorry"}</p>
+      <p className="text-lg text-gray-600 mb-8">
+        {message ? message : "sorry"}
+      </p>
       <Link
         href="/"
         className="bg-zinc-700 text-gray-200 p-3 border-1 rounded-xl uppercase font-bold hover:opacity-50 duration-100"
